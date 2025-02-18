d857e2d5e642c5530d2ff2f6f5b2d01f9282be26
raiganm <raigan.magallan@g.austincc.edu>
2025-02-18 01:47:28 +0000

    get sum of numbers

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -2,7 +2,11 @@ import repetition
 
 
 def main():
-    repetition.use_a_while_loop(3)
+    num = input("Enter a number: ") #captured from keyboard as a string
+
+    result = repetition.get_sum_of_squares(int(num)) #int means convert num to a number
+
+    print(result)
  
 #run the main function
 main()
--- src/examples/d_repetition/repetition.py
+++ src/examples/d_repetition/repetition.py
@@ -9,3 +9,19 @@ def use_a_while_loop(num):
         print(counter, counter < num, 'Hello')
         #statement that makes boolean expression false
         counter = counter + 1
+        if(counter == 3):
+            print(counter, counter < num, '')
+
+#3  1*1 + 2*2 + 3*3
+#4  1*1 + 2*2 + 3*3 + 4*4
+#5  1*1 + 2*2 + 3*3 + 4*4 + 5*5
+
+def get_sum_of_squares(num): #4
+
+    sum = 0
+
+    while(num > 0):
+        sum = sum + num * num
+        num = num - 1 #will make num > 0 false
+
+    return sum
