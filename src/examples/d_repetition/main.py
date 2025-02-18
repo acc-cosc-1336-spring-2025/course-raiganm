bf4ebc48b7136cb5ec1264bd80a9e727c806264e
raiganm <raigan.magallan@g.austincc.edu>
2025-02-18 01:27:07 +0000

    use while loop 3

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,2 +1,8 @@
 import repetition
 
+
+def main():
+    repetition.use_a_while_loop(3)
+ 
+#run the main function
+main()
--- src/examples/d_repetition/repetition.py
+++ src/examples/d_repetition/repetition.py
@@ -1,2 +1,11 @@
 def test_config():
     return True
+
+def use_a_while_loop(num):
+
+    counter = 0
+
+    while(counter < num): #boolean expressions loop while true, if stop while false
+        print(counter, counter < num, 'Hello')
+        #statement that makes boolean expression false
+        counter = counter + 1
