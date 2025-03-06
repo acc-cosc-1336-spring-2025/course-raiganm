1b7b72026b10d7c26c8e3ddba7b49d8579d82a8f
raiganm <raigan.magallan@g.austincc.edu>
2025-03-06 01:22:02 +0000

    string built-in testing methods

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -2,6 +2,8 @@
 import strings
 
 def main():
+    lang = "Python"
+    lang.
     strings.string_loop_w_for("Python")
 
 
--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -45,3 +45,20 @@ class Test_Config(unittest.TestCase):
         not_in = 'Seven' not in text
 
         self.assertEqual(not_in, True)
+
+    def test_string_isdigit(self):
+        str = "123"
+
+        self.assertEqual(str.isdigit(), True)
+
+    def test_string_not_isdigit(self):
+        str = "OneTwoThree"
+
+        self.assertEqual(str.isdigit(), False)
+
+    def test_string_isupper(self):
+        str = "OneTwoThree"
+
+        self.assertEqual(str.isupper(), False)
+
+    
